5ea0ddcbd23a990feca0606cfdb952ccea743b14
Grenadator <grenadator@gmail.com>
2020-04-18 15:31:03 +0200

    refactor: remove console logs & use voice category id from env file

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,8 @@ voiceChatBot.on('message', (msg) => {
 
 voiceChatBot.on('voiceStateUpdate', async (oldState, newState) => {
   if (newState.channelID === process.env.CREATING_CHANNEL_ID) {
-    console.log(oldState, newState);
     const creator = await newState.guild.members.fetch(newState.id);
-    console.log(creator);
-    newState.guild.channels.create(`${creator.user.username}'s channel`, { type: 'voice', parent: '562227213185974285' })
+    newState.guild.channels.create(`${creator.user.username}'s channel`, { type: 'voice', parent: process.env.VOICE_CATEGORY_ID })
   };
 });
 
