f852c1c23387915ca1c383885b88435089a992c6
Grenadator <grenadator@gmail.com>
2020-04-18 14:54:07 +0200

    feat: allow a user to create a voice channel

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,16 @@ dotenv.config();
 const voiceChatBot = new discord.Client();
 
 voiceChatBot.on('message', (msg) => {
-  msg.channel.send('rofl tg');
+  // msg.channel.send('rofl tg');
+});
+
+voiceChatBot.on('voiceStateUpdate', async (oldState, newState) => {
+  if (newState.channelID === process.env.CREATING_CHANNEL_ID) {
+    console.log(oldState, newState);
+    const creator = await newState.guild.members.fetch(newState.id);
+    console.log(creator);
+    newState.guild.channels.create(`${creator.user.username}'s channel`, { type: 'voice', parent: '562227213185974285' })
+  };
 });
 
 voiceChatBot.login(process.env.TOKEN);
