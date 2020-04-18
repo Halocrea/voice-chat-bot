7811647d2a1849d3f59327c814015c23dbd7774b
Grenadator <grenadator@gmail.com>
2020-04-18 18:55:44 +0200

    feat: delete a channel when a user leave it if empty

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,33 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 const voiceChatBot = new discord.Client();
 
-voiceChatBot.on('message', (msg) => {
+voiceChatBot.on('message', msg => {
   // msg.channel.send('rofl tg');
 });
 
 voiceChatBot.on('voiceStateUpdate', async (oldState, newState) => {
+  // Create a voice channel when a user join the "creating" channel
   if (newState.channelID === process.env.CREATING_CHANNEL_ID) {
+    // We create the channel
     const creator = await newState.guild.members.fetch(newState.id);
     newState.guild.channels.create(`${creator.user.username}'s channel`, { type: 'voice', parent: process.env.VOICE_CATEGORY_ID })
-  };
+    // TODO: Then we move the user to the new channel
+
+  } else if (oldState.channelID) { // Having null as an old state channel id means that the user wasn't in a vocal channel before
+    const channelLeft = newState.guild.channels.resolve(oldState.channelID!);
+    let memberCount = 0;
+    for (const member of channelLeft!.members)
+      memberCount++
+    if (
+      channelLeft &&
+      !memberCount &&
+      channelLeft.id !== process.env.CREATING_CHANNEL_ID
+    ) {
+      channelLeft.delete()
+      .then(() => console.log('channel deleted'))
+      .catch(console.error);
+    }
+  }
 });
 
 voiceChatBot.login(process.env.TOKEN);
